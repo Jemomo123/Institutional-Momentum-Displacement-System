ed666b31fb28f771aa26b5a56edff48bb273bd8b
Jemomo123 <blessedmosomi@gmail.com>
2026-05-31 08:32:32 +0300

    Correct parameter order in TemplateResponse
    Fix template response call by correcting the parameter order.

--- server.py
+++ server.py
@@ -65,8 +65,7 @@ async def startup_event():
 
 @app.get("/", response_class=HTMLResponse)
 async def desktop_gateway(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
+    return templates.TemplateResponse(request, "index.html")
 @app.get("/stream/signals")
 async def stream_signals(request: Request):
     async def event_generator():
